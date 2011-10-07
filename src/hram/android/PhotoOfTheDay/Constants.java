7f525e2a9919c01da4ef0ebbab9d07ff326fe272
hram.ov.e@gmail.com <hram.ov.e@gmail.com@bc4827b3-a5a1-6646-df9e-ec12b04024f4>
2011-10-07 22:05:45 +0000

    !!! версия 1.10

--- src/hram/android/PhotoOfTheDay/Constants.java
+++ src/hram/android/PhotoOfTheDay/Constants.java
@@ -7,6 +7,7 @@ public abstract class Constants
 	public static final String LAST_UPDATE = "last_update";
 	public static final String LAST_URL = "last_url";
 	public static final String SOURCES_NAME = "sources";
+	public static final String WIFI_ONLY = "wifiOnly";
 	
 	public static final long UPDATE_INTERVAL = 60 * 60 * 1000;
 }
